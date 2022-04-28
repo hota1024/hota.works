88bd2f32bceb75f09cebf430b6ade12509ba59b3
= <hotalog@hota1024.com>
2022-04-28 23:37:46 +0900

    💄add global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
+import { globalCss } from '@/stitches.config'
 import type { AppProps } from 'next/app'
 
+const globalStyles = globalCss({
+  '*': { margin: 0, padding: 0, boxSizing: 'border-box' },
+})
+
 function MyApp({ Component, pageProps }: AppProps) {
+  globalStyles()
+
   return (
     <>
       <Component {...pageProps} />
