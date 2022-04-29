91ef3ffab3614cf458b48509018f1d63b0af07eb
hota1024 <hotalog@hota1024.com>
2022-04-29 15:50:33 +0900

    💄fix stack spacing

--- src/components/Stack.tsx
+++ src/components/Stack.tsx
@@ -2,67 +2,153 @@ import { styled } from '@/stitches.config'
 
 export const Stack = styled('div', {
   display: 'flex',
-  '& > *': {
-    margin: 0,
-  },
   variants: {
+    spacing: {
+      0: {},
+      1: {},
+      2: {},
+      3: {},
+      4: {},
+      5: {},
+      6: {},
+    },
     direction: {
       row: {
         flexDirection: 'row',
-        '& > *': {
-          marginTop: 0,
-        },
       },
       column: {
         flexDirection: 'column',
-        marginLeft: 0,
-      },
-    },
-    spacing: {
-      0: {
-        '& > *': {
-          marginLeft: 0,
-          marginTop: 0,
-        },
-      },
-      1: {
-        '& > *': {
-          marginLeft: '$1',
-          marginTop: '$1',
-        },
-      },
-      2: {
-        '& > *': {
-          marginLeft: '$2',
-          marginTop: '$2',
-        },
-      },
-      3: {
-        '& > *': {
-          marginLeft: '$3',
-          marginTop: '$3',
-        },
-      },
-      4: {
-        '& > *': {
-          marginLeft: '$4',
-          marginTop: '$4',
-        },
-      },
-      5: {
-        '& > *': {
-          marginLeft: '$5',
-          marginTop: '$5',
-        },
-      },
-      6: {
-        '& > *': {
-          marginLeft: '$6',
-          marginTop: '$6',
-        },
       },
     },
   },
+  compoundVariants: [
+    {
+      direction: 'row',
+      spacing: 0,
+      css: {
+        ':not(:first-child)': {
+          marginLeft: 0,
+        },
+      },
+    },
+    {
+      direction: 'row',
+      spacing: 1,
+      css: {
+        ':not(:first-child)': {
+          marginLeft: '$1',
+        },
+      },
+    },
+    {
+      direction: 'row',
+      spacing: 2,
+      css: {
+        ':not(:first-child)': {
+          marginLeft: '$2',
+        },
+      },
+    },
+    {
+      direction: 'row',
+      spacing: 3,
+      css: {
+        ':not(:first-child)': {
+          marginLeft: 3,
+        },
+      },
+    },
+    {
+      direction: 'row',
+      spacing: 4,
+      css: {
+        ':not(:first-child)': {
+          marginLeft: 4,
+        },
+      },
+    },
+    {
+      direction: 'row',
+      spacing: 5,
+      css: {
+        ':not(:first-child)': {
+          marginLeft: 5,
+        },
+      },
+    },
+    {
+      direction: 'row',
+      spacing: 6,
+      css: {
+        ':not(:first-child)': {
+          marginLeft: 6,
+        },
+      },
+    },
+    {
+      direction: 'column',
+      spacing: 0,
+      css: {
+        ':not(:first-child)': {
+          marginTop: 0,
+        },
+      },
+    },
+    {
+      direction: 'column',
+      spacing: 1,
+      css: {
+        ':not(:first-child)': {
+          marginTop: 1,
+        },
+      },
+    },
+    {
+      direction: 'column',
+      spacing: 2,
+      css: {
+        ':not(:first-child)': {
+          marginTop: 2,
+        },
+      },
+    },
+    {
+      direction: 'column',
+      spacing: 3,
+      css: {
+        ':not(:first-child)': {
+          marginTop: 3,
+        },
+      },
+    },
+    {
+      direction: 'column',
+      spacing: 4,
+      css: {
+        ':not(:first-child)': {
+          marginTop: 4,
+        },
+      },
+    },
+    {
+      direction: 'column',
+      spacing: 5,
+      css: {
+        ':not(:first-child)': {
+          marginTop: 5,
+        },
+      },
+    },
+    {
+      direction: 'column',
+      spacing: 6,
+      css: {
+        ':not(:first-child)': {
+          marginTop: 6,
+        },
+      },
+    },
+  ],
   defaultVariants: {
     direction: 'column',
   },
