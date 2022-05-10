49c3644cb302a5f330b05efb067518a8340bb1e2
hota1024 <hotalog@hota1024.com>
2022-05-10 13:59:59 +0900

    💄change animation speed

--- src/components/DotRectangle.tsx
+++ src/components/DotRectangle.tsx
@@ -15,9 +15,9 @@ export const DotRectangle = styled('div', {
   width: 256,
   height: 256,
   backgroundImage: 'radial-gradient($blue 2px, transparent 0)',
-  backgroundSize: '32px 32px',
+  backgroundSize: '16px 16px',
   backgroundPosition: '0px 0px',
   fontFamily: `'Roboto Mono', 'Noto Sans JP', sans-serif`,
   color: '$text',
-  animation: `${dotMove} 3000ms infinite linear`,
+  animation: `${dotMove} 10000ms infinite linear`,
 })
